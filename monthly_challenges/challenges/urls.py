172a62fd05bc60536c5c6a4963d71a549ae352e5
magicc11 <martanddestiny@gmail.com>
2024-04-14 01:26:39 +0530

    path converters

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 # adding path details here
 urlpatterns = [
-    path("january", views.index)
+    #path("january", views.index_jan),
+    #path("february", views.index_feb),
+    # Here the value inside <> act as a keyword argument for the corresponding view i.e monthly_challenge
+    path("<int:month_chosen>", views.monthly_challenge_by_num),
+    path("<str:month_chosen>", views.monthly_challenge),
     ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("This works")
+def index_jan(request):
+    return HttpResponse("This works- January")
+
+
+def index_feb(request):
+    return HttpResponse("This works - February")
+
+
+def monthly_challenge(request, month_chosen):
+    if month_chosen.lower() == "january":
+        challenge_text = "This works- January"
+    elif month_chosen.lower() == "february":
+        challenge_text = "This works- February"
+    elif month_chosen.lower() == "march":
+        challenge_text = "This works- March"
+    else:
+        return HttpResponseNotFound("This Month is Not supported")
+    
+    return HttpResponse(challenge_text)
+
+
+
+def monthly_challenge_by_num(request, month_chosen):
+    if month_chosen== 1:
+        challenge_text = "This works- January"
+    elif month_chosen== 2:
+        challenge_text = "This works- February"
+    elif month_chosen== 3:
+        challenge_text = "This works- March"
+    else:
+        return HttpResponseNotFound("This Month is Not supported")
+    
+    return HttpResponse(challenge_text)
