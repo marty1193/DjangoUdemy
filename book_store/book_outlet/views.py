20782148de4d28d10f8b6057492ff3ca43fea8c6
magicc11 <martanddestiny@gmail.com>
2024-06-23 02:59:34 +0530

    Aggegations and order by

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+from .models import Book
+from django.http import Http404
+from django.db.models import Avg
+
 # Create your views here.
+
+def index(request):
+    books=Book.objects.all().order_by("-title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating")) 
+    return render(request, "book_outlet/index.html", 
+                  {"books_list":books, 
+                   "total_number_of_books":num_books,
+                   "average_rating":avg_rating["rating__avg"]})
+
+
+def book_detail(request, id):
+    try:
+        book=Book.objects.get(pk=id)
+    except:
+        raise Http404("This Book Doesnot Exists !")
+    return render(request, "book_outlet/book_detail.html",
+                  {"title": book.title,
+                   "aurthor": book.aurthor,
+                   "rating": book.rating,
+                   "is_bestseller": book.is_bestselling})
