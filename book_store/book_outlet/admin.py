82bc7836705ad0fb8795feea708be2b23dd87265
magicc11 <martanddestiny@gmail.com>
2024-06-25 23:56:47 +0530

    Relationship Data Model

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Aurthor
 
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
-    readonly_fields = ('rating',)
     list_filter = ('rating',)
     list_display =('title','rating','aurthor')
 
+class AurthorAdmin(admin.ModelAdmin):
+
+    #list_filter = ('aurthor',)
+    list_display =('first_name', 'last_name')
+
 admin.site.register(Book, BookAdmin)
+admin.site.register(Aurthor, AurthorAdmin)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -3,12 +3,30 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=5)
+    city = models.CharField(max_length=50)
+
+
+class Aurthor(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE,null=True)
+
+
+    def full_name(self):
+        return (f"{self.first_name} {self.last_name}")
+
+    def __str__(self):
+        return self.full_name()
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)])
-    aurthor = models.CharField(null=True, max_length=100)
+    aurthor = models.ForeignKey(Aurthor, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
 
     def __str__(self):
